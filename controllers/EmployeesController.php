7af3e44f90fe848078521087dfc604a9f1f924d5
pepe <pepe@mail.ru>
2021-09-08 18:34:02 +0300

    Очистка SiteController, создание EmployeesController

---
+++ controllers/EmployeesController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Employees;
+use app\models\EmployeesSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use Yii;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+class EmployeesController extends Controller
+{
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function actionIndex()
+    {
+        $searchModel = new EmployeesSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $model = new Employees();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = Employees::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    public function actionMakeReport(){
+        $employees = Employees::find()->all();
+        $tmpFile = Yii::getAlias('@app') . "/views/employees/reports/report.php";
+        $content = $this->renderFile($tmpFile, ['employees' => $employees]);
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHtml($content);
+        $mpdf->Output('salaryReport.pdf', \Mpdf\Output\Destination::DOWNLOAD);
+    }
+}
